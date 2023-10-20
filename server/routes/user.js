546cd256514fd4d3337296fd07df6d3f80080b48
truongdv <truongdv@relipasoft.com>
2023-10-21 04:47:10 +0700

    commit 21/10/2023

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/user')
+const { verifyAccessToken } = require('../middlewares/verifyToken')
 
 router.post('/register', ctrls.register)
+router.post('/login', ctrls.login)
+router.get('/current', verifyAccessToken, ctrls.getCurrent)
+router.post('/refreshtoken', ctrls.refreshAccessToken)
+router.post('/logout', ctrls.logout)
+router.put('/resetpassword', ctrls.resetPassword)
 
 module.exports = router
