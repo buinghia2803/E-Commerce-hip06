3a5d196decb4e08dab7571b22fc980835c676abf
truongdv <truongdv@relipasoft.com>
2023-10-22 05:04:57 +0700

    commit 22/10/2023

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -23,11 +23,55 @@ const getProduct = asyncHandler(async (req, res) => {
 })
 // Filtering, sorting and pagination
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find()
-  return res.status(200).json({
-    success: products ? true : false,
-    productData: products ? products : 'Cannot get products'
-  })
+  const queries = { ...req.query }
+
+  // Tách các trường đặc biệt ra khỏi query
+  const excludeFields = ['limit', 'sort', 'page', 'fields']
+  excludeFields.forEach(el => delete queries[el])
+
+  // Format lại các operators cho đún cú pháp mongoose
+  let queryString = JSON.stringify(queries)
+  queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, matchedEl => `$${matchedEl}`)
+  const formatedQueries = JSON.parse(queryString)
+
+  // Filtering
+  if (queries?.title) formatedQueries.title = { $regex: queries.title, $options: 'i' }
+  let queryCommand = Product.find(formatedQueries)
+
+  // Sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ')
+    queryCommand = queryCommand.sort(sortBy)
+  }
+
+  // Fields limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ')
+    queryCommand = queryCommand.select(fields)
+  }
+
+  // Pagination
+  // limit: số object lấy về 1 lần gọi api 
+  // skip: bỏ phần tử
+  const page = +req.query.page || 1
+  const limit = +req.query.limit || process.env.LIMIT_PRODUCTS
+  const skip = (page - 1) * limit
+  queryCommand.skip(skip).limit(limit)
+
+  // Execute query
+  // Số lượng sp thỏa mãn đk !== số lượng sp trả về một lần api
+
+  queryCommand.then(async (response) => {
+    const counts = await Product.find(formatedQueries).countDocuments()
+
+    return res.status(200).json({
+      success: response ? true : false,
+      counts,
+      products: response ? response : 'Cannot get products'
+    })
+  }).catch((err) => {
+    if (err) throw new Error(err.message)
+  });
 })
 
 const updateProduct = asyncHandler(async (req, res) => {
@@ -49,10 +93,44 @@ const deleteProduct = asyncHandler(async (req, res) => {
   })
 })
 
+const ratings = asyncHandler(async (req, res) => {
+  const { _id } = req.user
+  const { star, comment, pid } = req.body
+  if (!star && !pid) throw new Error('Missing inputs')
+  const ratingProduct = await Product.findById(pid)
+  const alreadyRating = ratingProduct?.ratings?.find(el => el.postedBy.toString() === _id)
+  if (alreadyRating) {
+    // update star & comment
+    await Product.updateOne({
+      ratings: { $elemMatch: alreadyRating }
+    }, {
+      $set: { "ratings.$.star": star, "ratings.$.comment": comment }
+    }, { new: true })
+  } else {
+    // add star & comment
+    await Product.findByIdAndUpdate(pid, {
+      $push: { ratings: { star, comment, postedBy: _id } }
+    }, { new: true })
+  }
+
+  // Sum ratings
+  const updatedProduct = await Product.findById(pid)
+  const ratingsCount = updatedProduct.ratings.length
+  const sumRatings = updatedProduct.ratings.reduce((sum, el) => sum + +el.star, 0)
+  updatedProduct.totalRatings = Math.round(sumRatings * 10 / ratingsCount) / 10
+  await updatedProduct.save()
+
+  return res.status(200).json({
+    success: true,
+    updatedProduct
+  })
+})
+
 module.exports = {
   createProduct,
   getProduct,
   getProducts,
   updateProduct,
-  deleteProduct
+  deleteProduct,
+  ratings
 }
