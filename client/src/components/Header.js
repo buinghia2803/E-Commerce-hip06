1a182a940b174c44dd7acfbbad29c617e5828686
truongdv <truongdv@relipasoft.com>
2023-10-31 00:57:46 +0700

    commit 31/10/2023

---
+++ client/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import icons from '../ultils/icons'
+import { Link } from "react-router-dom"
+import path from '../ultils/path'
+
+const { RiPhoneFill, MdEmail, BsHandbagFill, FaUserCircle } = icons
+const Header = () => {
+  return (
+    <div className='border w-main flex justify-between h-[110px] py-[35px]'>
+      <Link to={`/${path.HOME}`}>
+        <img src={logo} alt="logo" className='w-[234px] object-contain' />
+      </Link>
+      <div className='flex text-[13px] '>
+        <div className='flex flex-col px-6 border-r items-center'>
+          <span className='flex gap-4 items-center'>
+            <RiPhoneFill color='red' />
+            <span className='font-semibold'>0967816629</span>
+          </span>
+          <span>Mon-D</span>
+        </div>
+        <div className='flex flex-col px-6 border-r items-center'>
+          <span className='flex gap-4 items-center'>
+            <MdEmail color='red' />
+            <span className='font-semibold'>Email</span>
+          </span>
+          <span>Online</span>
+        </div>
+        <div className='flex items-center justify-center gap-2 px-6 border-r'>
+          <BsHandbagFill />
+          <span>0 item(s)</span>
+        </div>
+        <div className='flex items-center justify-center px-6'>
+          <FaUserCircle size={24} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ data/cate_brand.js
@@ -0,0 +1,12 @@
+const data = [
+  {
+    cate: 'Smartphone',
+    brand: ['Apple', 'Samsung']
+  },
+  {
+    cate: 'Tablet',
+    brand: ['Ipad', 'Samsung']
+  }
+]
+
+module.exports = data
