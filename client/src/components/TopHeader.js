476e216c6510fa4a142171009b9d8116fc2c6fdf
truongdv <truongdv@relipasoft.com>
2023-11-10 19:34:43 +0700

    commit 10/11/2023

--- client/src/components/TopHeader.js
+++ client/src/components/TopHeader.js
@@ -7,7 +7,7 @@ const TopHeader = () => {
     <div className='h-[38px] w-full bg-main flex items-center justify-center'>
       <div className='w-main flex items-center justify-between text-xs text-white'>
         <span>Order online (0967816629)</span>
-        <Link to={`/${path.LOGIN}`}>Sign In</Link>
+        <Link className='hover:text-gray-800' to={`/${path.LOGIN}`}>Sign In</Link>
       </div>
     </div>
   )
--- client/src/pages/public/Login.js
+++ client/src/pages/public/Login.js
@@ -1,8 +1,109 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import { InputField, Button } from '../../components'
+import { apiRegister, apiLogin } from '../../apis/user'
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom'
+import path from '../../ultils/path'
+import { register } from "../../store/user/userSlice";
+import { useDispatch } from 'react-redux'
 
 const Login = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [payload, setPayload] = useState({
+    email: '',
+    password: '',
+    firstname: '',
+    lastname: '',
+    mobile: ''
+  })
+  const [isRegister, setIsRegister] = useState(false)
+  const resetPayload = () => {
+    setPayload({
+      email: '',
+      password: '',
+      firstname: '',
+      lastname: '',
+      mobile: ''
+    })
+  }
+  const handleSubmit = useCallback(async () => {
+    const { firstname, lastname, mobile, ...data } = payload
+
+    if (isRegister) {
+      const response = await apiRegister(payload)
+      if (response.success) {
+        Swal.fire('Congratulation', response.mes, 'success').then(() => {
+          setIsRegister(false)
+          resetPayload()
+        })
+      } else Swal.fire('Oops!', response.mes, 'error')
+    } else {
+      const rs = await apiLogin(data)
+      if (rs.success) {
+        dispatch(register({ isLoggedIn: true, token: rs.accessToken, current: rs.mes }))
+        navigate(`/${path.HOME}`)
+      } else Swal.fire('Oops!', rs.mes, 'error')
+    }
+  }, [payload, isRegister])
+
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen relative'>
+      <img
+        src=""
+        alt=""
+        className='w-full h-full object-cover'
+      />
+      <div className='absolute top-0 bottom-0 left-0 right-1/2 flex items-center justify-center'>
+        <div className='p-8 bg-white flex flex-col items-center rounded-md min-w-[500px]'>
+          <h1 className='text-[28px] font-semibold text-main mb-8'>{isRegister ? 'Register' : 'Login'}</h1>
+          {isRegister && <div className='flex items-center gap-2'>
+            <InputField
+              value={payload.firstname}
+              setValue={setPayload}
+              nameKey='firstname'
+            />
+            <InputField
+              value={payload.lastname}
+              setValue={setPayload}
+              nameKey='lastname'
+            />
+          </div>}
+          <InputField
+            value={payload.email}
+            setValue={setPayload}
+            nameKey='email'
+          />
+          {isRegister && <InputField
+            value={payload.mobile}
+            setValue={setPayload}
+            nameKey='mobile'
+          />}
+          <InputField
+            value={payload.password}
+            setValue={setPayload}
+            nameKey='password'
+            type='password'
+          />
+          <Button
+            name={isRegister ? 'Register' : 'Login'}
+            handleOnClick={handleSubmit}
+            fw
+          />
+          <div className='flex items-center justify-between w-full text-sm my-2'>
+            {!isRegister && <span className='text-blue-500 hover:underline cursor-pointer'>Forgot acc</span>}
+            {!isRegister && <span
+              className='text-blue-500 hover:underline cursor-pointer'
+              onClick={() => setIsRegister(true)}
+            >Create acc</span>}
+            {isRegister && <span
+              className='text-blue-500 hover:underline cursor-pointer w-full text-center'
+              onClick={() => setIsRegister(false)}
+            >Go Login</span>}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
