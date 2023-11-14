868719e4c9d3f00ff3d499a574ade98bde39acb2
truongdv <truongdv@relipasoft.com>
2023-11-15 00:59:14 +0700

    commit 15/11/2023

--- client/src/pages/public/Login.js
+++ client/src/pages/public/Login.js
@@ -1,12 +1,13 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { InputField, Button } from '../../components'
-import { apiRegister, apiLogin, apiForgotPassword } from '../../apis/user'
+import { apiRegister, apiLogin, apiForgotPassword, apiFinalRegister } from '../../apis/user'
 import Swal from 'sweetalert2'
 import { useNavigate } from 'react-router-dom'
 import path from '../../ultils/path'
-import { register } from "../../store/user/userSlice";
+import { login } from "../../store/user/userSlice";
 import { useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
+import { validate } from '../../ultils/helpers'
 
 const Login = () => {
   const navigate = useNavigate()
@@ -18,6 +19,8 @@ const Login = () => {
     lastname: '',
     mobile: ''
   })
+  const [isVerifiedEmail, setIsVerifiedEmail] = useState(false)
+  const [invalidFields, setInvalidFields] = useState([])
   const [isRegister, setIsRegister] = useState(false)
   const [isForgotPassword, setIsForgotPassword] = useState(false)
   const resetPayload = () => {
@@ -29,6 +32,7 @@ const Login = () => {
       mobile: ''
     })
   }
+  const [token, setToken] = useState('')
   const [email, setEmail] = useState('')
   const handleForgotPassword = async () => {
     const response = await apiForgotPassword({ email })
@@ -36,28 +40,63 @@ const Login = () => {
       toast.success(response.mes, { theme: 'colored' })
     } else toast.info(response.mes, { theme: 'colored' })
   }
+
+  useEffect(() => {
+    resetPayload()
+  }, [isRegister])
+
+  // SUBMIT
   const handleSubmit = useCallback(async () => {
     const { firstname, lastname, mobile, ...data } = payload
 
-    if (isRegister) {
-      const response = await apiRegister(payload)
-      if (response.success) {
-        Swal.fire('Congratulation', response.mes, 'success').then(() => {
-          setIsRegister(false)
-          resetPayload()
-        })
-      } else Swal.fire('Oops!', response.mes, 'error')
-    } else {
-      const rs = await apiLogin(data)
-      if (rs.success) {
-        dispatch(register({ isLoggedIn: true, token: rs.accessToken, current: rs.mes }))
-        navigate(`/${path.HOME}`)
-      } else Swal.fire('Oops!', rs.mes, 'error')
+    const invalids = isRegister ? validate(payload, setInvalidFields) : validate(data, setInvalidFields)
+    if (invalids === 0) {
+      if (isRegister) {
+        const response = await apiRegister(payload)
+        if (response.success) {
+          setIsVerifiedEmail(true)
+        } else Swal.fire('Oops!', response.mes, 'error')
+      } else {
+        const rs = await apiLogin(data)
+        if (rs.success) {
+          dispatch(login({ isLoggedIn: true, token: rs.accessToken, current: rs.mes }))
+          navigate(`/${path.HOME}`)
+        } else Swal.fire('Oops!', rs.mes, 'error')
+      }
     }
   }, [payload, isRegister])
 
+  const finalRegister = async () => {
+    const response = await apiFinalRegister(token)
+    if (response.success) {
+      Swal.fire('Congratulation', response.mes, 'success').then(() => {
+        setIsRegister(false)
+        resetPayload()
+      })
+    } else Swal.fire('Oops!', response.mes, 'error')
+    setIsVerifiedEmail(false)
+    setToken('')
+  }
+
   return (
     <div className='w-screen h-screen relative'>
+      {isVerifiedEmail && <div className='absolute top-0 left-0 right-0 bottom-0 bg-overlay z-50 flex flex-col justify-center items-center'>
+        <div className='bg-white w-[500px] rounded-md p-8'>
+          <h4>We sent a code to your mail. Please check your mail and enter your code:</h4>
+          <input type="text"
+            value={token}
+            onChange={e => setToken(e.target.value)}
+            className='p-2 border rounded-md outline-none'
+          />
+          <button
+            ype='button'
+            className='px-4 py-2 bg-blue-500 font-semibold text-white rounded-md ml-4'
+            onClick={finalRegister}
+          >
+            Submit
+          </button>
+        </div>
+      </div>}
       {isForgotPassword && <div className='absolute animate-slide-right top-0 left-0 bottom-0 right-0 bg-white flex flex-col items-center py-8 z-50'>
         <div className='flex flex-col gap-4'>
           <label htmlFor="email">Enter your email</label>
@@ -96,28 +135,38 @@ const Login = () => {
               value={payload.firstname}
               setValue={setPayload}
               nameKey='firstname'
+              invalidFields={invalidFields}
+              setInvalidFields={setInvalidFields}
             />
             <InputField
               value={payload.lastname}
               setValue={setPayload}
               nameKey='lastname'
+              invalidFields={invalidFields}
+              setInvalidFields={setInvalidFields}
             />
           </div>}
           <InputField
             value={payload.email}
             setValue={setPayload}
             nameKey='email'
+            invalidFields={invalidFields}
+            setInvalidFields={setInvalidFields}
           />
           {isRegister && <InputField
             value={payload.mobile}
             setValue={setPayload}
             nameKey='mobile'
+            invalidFields={invalidFields}
+            setInvalidFields={setInvalidFields}
           />}
           <InputField
             value={payload.password}
             setValue={setPayload}
             nameKey='password'
             type='password'
+            invalidFields={invalidFields}
+            setInvalidFields={setInvalidFields}
           />
           <Button
             name={isRegister ? 'Register' : 'Login'}
